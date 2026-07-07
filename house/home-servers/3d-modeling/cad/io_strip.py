485f465aa5d9449b7090701d098b448f16badbfd
Serhii Sliepchenko <sergey.slepchenko@gmail.com>
2026-07-07 12:16:36 +0300

    3d-modeling: io_strip — розрив ніжки-упора навколо портів знизу плати (TF)

--- house/home-servers/3d-modeling/cad/io_strip.py
+++ house/home-servers/3d-modeling/cad/io_strip.py
@@ -26,6 +26,11 @@ def build():
         with BuildSketch(Plane.XZ) as foot:
             with Locations(((x0 + x1) / 2, P.BOARD_Z - 2.0)):
                 Rectangle(x1 - x0, 4.0)
+            # розриви ніжки: TF-слот знизу плати (і всі порти з bz<0)
+            for name, kind, bx, bz, w, h in P.IO_PORTS:
+                if kind == "rect" and bz < 0:
+                    with Locations((P.IO_BOARD_X0 + bx, P.BOARD_Z - 2.0)):
+                        Rectangle(w + 6.0, 4.2, mode=Mode.SUBTRACT)
         extrude(foot.sketch, amount=-3.0)
     return sp.part
 
